e9ef8a34b4a6e4513d396d9125cde25917e3d267
RRuby2 <becca.ruby@gmail.com>
2020-09-25 15:00:53 -0700

    my submission of cachematrix.R for ProgrammingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## A set of functions that create a matrix and then create, store
+## and return the inverse of the matrix. 
 
-## Write a short comment describing this function
+## Creates a matrix and a list of functions that are used to set and 
+## get the values of the matrix and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
 
+    inv <- NULL
+    set <- function(y) {
+      x <<- y
+      inv <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inv_m) inv <<- inv_m
+    getinv <- function() inv
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
+  
 }
 
 
-## Write a short comment describing this function
+## Returns the inverse of the matrix created with makeCacheMatrix()
+## First, checks to see if there is a value stored for the inverse matrix 'inv'
+## if yes, a message is displayed and the cached inverse matrix is returned
+## otherwise, the function creates the inverse matrix and returns it
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+    if(!is.null(inv)) {
+      message("getting cached inverse matrix")
+      return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setinv(inv)
+    inv
+        
 }
